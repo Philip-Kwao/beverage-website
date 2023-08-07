701d2a8cee3434df7a1712ac35519874d3843083
Philip-Kwao <kwaoizy@gmail.com>
2023-08-07 18:51:43 +0000

    Updated Gallery

--- src/components/project-reference/Gallery.jsx
+++ src/components/project-reference/Gallery.jsx
@@ -5,6 +5,8 @@ import 'lightgallery/css/lightgallery.css';
 import 'lightgallery/css/lg-zoom.css';
 import 'lightgallery/css/lg-thumbnail.css';
 
+import Img1 from '../../assets/images/jcl/1.jpg'
+
 // If you want you can use SCSS instead of css
 // import 'lightgallery/scss/lightgallery.scss';
 // import 'lightgallery/scss/lg-zoom.scss';
@@ -20,12 +22,13 @@ import { swipeDetails } from '@/data/SlideData';
 import {Img} from "@/assets/images/sc/1.jpg"
 import { useState } from 'react';
 import {motion} from 'framer-motion'
-const pathname = usePathname()
+// const pathname = usePathname()
 
 const Gallery = () => {
     const onInit = () => {
         console.log('lightGallery has been initialized');
     };
+    //FIltering
     const [items, setItems] = useState(images)
     const filterItem = (catItem) => {
         const updatedItem = images.filter((cat) =>{
@@ -33,32 +36,45 @@ const Gallery = () => {
         })
         setItems(updatedItem)
     } 
+
+    const [showImages, setShowImages] = useState()
+    
+    const showImage = () => {}
+    
+        const imageShown = () => {
+            
+        }
+    
     return (
     <div className="App">
         <div className="flex flex-wrap items-center justify-around max-w-[20vw] mx-auto text-sm font-light my-6">
-            <button onClick={()=>{filterItem('one')}} className="px-4 py-2 rounded-full bg-orange-500 hover:bg-slate-800 text-white duration-200 ease-in-out">Project One</button>
-            <button onClick={()=>{filterItem('two')}} className="px-4 py-2 rounded-full bg-orange-500 hover:bg-slate-800 text-white duration-200 ease-in-out">Project Two</button>
-            <button onClick={()=>setItems(images)} className="px-4 py-2 rounded-full bg-orange-500 hover:bg-slate-800 text-white duration-200 ease-in-out">All</button>
+            <button onClick={()=>{filterItem('one')}} className="px-4 py-2 rounded-full hover:bg-orange-500 bg-slate-800 text-white duration-200 ease-in-out">Project One</button>
+            <button onClick={()=>{filterItem('two')}} className="px-4 py-2 rounded-full hover:bg-orange-500 bg-slate-800 text-white duration-200 ease-in-out">Project Two</button>
+            <button onClick={()=>setItems(images)} className="px-4 py-2 rounded-full hover:bg-orange-500 bg-slate-800 text-white duration-200 ease-in-out">All</button>
         </div>
-        <div className="flex items-center justify-center flex-wrap">
             {/* <LightGallery
                 onInit={onInit}
                 speed={500}
                 plugins={[lgThumbnail, lgZoom]}
             > */}
+                <div className="flex items-center justify-center flex-wrap relative">
                     {
                         items.map((image, index)=>(
                             <motion.div
                                 layout
                             >
-                                <a key={index} href={`/items/${index}`}>
-                                    <Image alt={index} src={image.img} width={300} height={500} className='w-[300px] h-[500px] object-cover hover:scale-75 duration-200 ease-in-out hover:shadow-lg' />
-                                </a>
+                                <motion.div key={index} 
+                                    whileTap={{scale:1.5}}
+                                    onClick={()=>setShowImages(image.img)}
+                                >
+                                    <Image onClick={()=>{showImage}} alt={index} src={image.img} width={300} height={500} className='w-[300px] h-[500px] object-cover hover:scale-75 duration-200 ease-in-out hover:shadow-lg' />
+                                </motion.div>
+                                {/* <Image src={showImages} className="w-full h-full absolute rounded-xl shadow-xl z-20" /> */}
                             </motion.div>
                         ))
                     }
-            {/* </LightGallery> */}
                 </div>
+            {/* </LightGallery> */}
         </div>
   )
 }
