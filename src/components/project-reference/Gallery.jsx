74852114269c3e5b09529a399539c3aaa2c659c5
Philip-Kwao <123540619+Philip-Kwao@users.noreply.github.com>
2023-08-09 10:35:32 +0000

    Update Gallery.jsx

--- src/components/project-reference/Gallery.jsx
+++ src/components/project-reference/Gallery.jsx
@@ -5,9 +5,12 @@ import 'lightgallery/css/lightgallery.css';
 import 'lightgallery/css/lg-zoom.css';
 import 'lightgallery/css/lg-thumbnail.css';
 
+import Img1 from '../../assets/images/jcl/1.jpg'
+
 // If you want you can use SCSS instead of css
 // import 'lightgallery/scss/lightgallery.scss';
 // import 'lightgallery/scss/lg-zoom.scss';
+import { usePathname } from 'next/navigation'
 
 // import plugins if you need
 import lgThumbnail from 'lightgallery/plugins/thumbnail';
@@ -17,23 +20,43 @@ import Image from 'next/image';
 import images from '@/data/gallery';
 import { swipeDetails } from '@/data/SlideData';
 import {Img} from "@/assets/images/sc/1.jpg"
+import { useState } from 'react';
 import {motion} from 'framer-motion'
+// const pathname = usePathname()
 
 const Gallery = () => {
     const onInit = () => {
         console.log('lightGallery has been initialized');
     };
-  return (
+    //FIltering
+    const [items, setItems] = useState(images)
+    const filterItem = (catItem) => {
+        const updatedItem = images.filter((cat) =>{
+            return cat.category === catItem
+        })
+        setItems(updatedItem)
+    } 
+
+    const [showImages, setShowImages] = useState()
+    
+    const showImage = () => {}
+    
+    return (
     <div className="App">
-        <div className="flex items-center justify-center flex-wrap">
+        <div className="flex flex-wrap items-center justify-around max-w-[20vw] mx-auto text-sm font-light my-6">
+            <button onClick={()=>{filterItem('one')}} className="px-4 py-2 rounded-full hover:bg-orange-500 bg-slate-800 text-white duration-200 ease-in-out">Project One</button>
+            <button onClick={()=>{filterItem('two')}} className="px-4 py-2 rounded-full hover:bg-orange-500 bg-slate-800 text-white duration-200 ease-in-out">Project Two</button>
+            <button onClick={()=>setItems(images)} className="px-4 py-2 rounded-full hover:bg-orange-500 bg-slate-800 text-white duration-200 ease-in-out">All</button>
+        </div>
             {/* <LightGallery
                 onInit={onInit}
                 speed={500}
                 plugins={[lgThumbnail, lgZoom]}
             > */}
+                <div className="flex items-center justify-center flex-wrap relative">
                     {
-                        images.map((image, index)=>(
-                             <motion.div
+                        items.map((image, index)=>(
+                            <motion.div
                                 layout
                                 key={image.id}
                             >
@@ -47,8 +70,8 @@ const Gallery = () => {
                             </motion.div>
                         ))
                     }
-            {/* </LightGallery> */}
                 </div>
+            {/* </LightGallery> */}
         </div>
   )
 }
