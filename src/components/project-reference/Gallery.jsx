53a8e9b8a85c78dec52d5565819b07f0c31ed83b
Philip-Kwao <123540619+Philip-Kwao@users.noreply.github.com>
2023-08-12 10:07:45 +0000

    Update Gallery.jsx

--- src/components/project-reference/Gallery.jsx
+++ src/components/project-reference/Gallery.jsx
@@ -18,6 +18,8 @@ import lgZoom from 'lightgallery/plugins/zoom';
 import Link from 'next/link';
 import Image from 'next/image';
 import images from '@/data/gallery';
+import images1 from '@/data/gallery';
+import images2 from '@/data/gallery';
 import { swipeDetails } from '@/data/SlideData';
 import {Img} from "@/assets/images/sc/1.jpg"
 import { useState } from 'react';
@@ -48,12 +50,12 @@ const Gallery = () => {
             <button onClick={()=>{filterItem('two')}} className="px-4 py-2 rounded-full hover:bg-orange-500 bg-slate-800 text-white duration-200 ease-in-out">Project Two</button>
             <button onClick={()=>setItems(images)} className="px-4 py-2 rounded-full hover:bg-orange-500 bg-slate-800 text-white duration-200 ease-in-out">All</button>
         </div>
+                <div className="flex items-center justify-center flex-wrap relative">
              <LightGallery
                 onInit={onInit}
                 speed={500}
                 plugins={[lgThumbnail, lgZoom]}
             > 
-                <div className="flex items-center justify-center flex-wrap relative">
                     {
                         items.map((image, index)=>(
                             <motion.a
@@ -69,8 +71,50 @@ const Gallery = () => {
                             </motion.a>
                         ))
                     }
-                </div>
              </LightGallery>
+             <LightGallery
+                onInit={onInit}
+                speed={500}
+                plugins={[lgThumbnail, lgZoom]}
+            > 
+                    {
+                        images1.map((image, index)=>(
+                            <motion.a
+                                layout
+                                key={image.id}
+                            >
+{/*                                 <motion.div 
+                                    whileTap={{scale:1.5}}
+                                > */}
+                                    <Image onClick={()=>{showImage}} alt={index} src={image.img} width={300} height={500} className='w-[300px] h-[500px] object-cover hover:scale-75 duration-200 ease-in-out hover:shadow-lg' />
+{/*                                 </motion.div> */}
+                                {/* <Image src={showImages} className="w-full h-full absolute rounded-xl shadow-xl z-20" /> */}
+                            </motion.a>
+                        ))
+                    }
+             </LightGallery>
+             <LightGallery
+                onInit={onInit}
+                speed={500}
+                plugins={[lgThumbnail, lgZoom]}
+            > 
+                    {
+                        images2.map((image, index)=>(
+                            <motion.a
+                                layout
+                                key={image.id}
+                            >
+{/*                                 <motion.div 
+                                    whileTap={{scale:1.5}}
+                                > */}
+                                    <Image onClick={()=>{showImage}} alt={index} src={image.img} width={300} height={500} className='w-[300px] h-[500px] object-cover hover:scale-75 duration-200 ease-in-out hover:shadow-lg' />
+{/*                                 </motion.div> */}
+                                {/* <Image src={showImages} className="w-full h-full absolute rounded-xl shadow-xl z-20" /> */}
+                            </motion.a>
+                        ))
+                    }
+             </LightGallery>
+                </div>
         </div>
   )
 }
