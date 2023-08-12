3fbbf2b07ad652187396a5600561bb530e84aa2f
Philip-Kwao <123540619+Philip-Kwao@users.noreply.github.com>
2023-08-12 08:43:23 +0000

    Update Gallery.jsx

--- src/components/project-reference/Gallery.jsx
+++ src/components/project-reference/Gallery.jsx
@@ -53,6 +53,7 @@ const Gallery = () => {
                 onInit={onInit}
                 speed={500}
                 plugins={[lgThumbnail, lgZoom]}
+                className="flex items-center justify-center flex-wrap relative"
             > 
                   
                     {
