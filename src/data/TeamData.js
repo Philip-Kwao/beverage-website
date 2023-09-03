672b3dec5422f6ee6dd7ae5189657fab9f56aafa
Philip-Kwao <123540619+Philip-Kwao@users.noreply.github.com>
2023-09-03 20:56:35 +0000

    Update TeamData.js

--- src/data/TeamData.js
+++ src/data/TeamData.js
@@ -1,4 +1,7 @@
 import CEO from '@/assets/images/jcl/ceo.jpeg'
+import Mary from '@/assets/images/jcl/IMG_1625.jpg'
+import Sam from '@/assets/images/jcl/IMG_1647.jpg'
+import Joe from '@/assets/images/jcl/IMG_1614.jpg'
 
 export const teamDetails = [ 
     {
