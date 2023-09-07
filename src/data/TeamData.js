50b615e0860ed675a49f4ac1f4c20c6973451731
Philip-Kwao <123540619+Philip-Kwao@users.noreply.github.com>
2023-09-07 13:46:42 +0000

    Update TeamData.js

--- src/data/TeamData.js
+++ src/data/TeamData.js
@@ -1,6 +1,7 @@
 import CEO from '@/assets/images/jcl/ceo.jpeg'
 import Grace from '@/assets/images/IMG_1625.jpg'
 import Sam from '@/assets/images/IMG_1647.jpg'
+import Joe from '@/assets/images/IMG_028.png'
 
 export const teamDetails = [ 
     {
